3ce717aaaeeeb2dc38bdfc8f5b456e97f8998d11
GFA03 <florinalexandru302003@gmail.com>
2024-04-06 11:22:24 +0300

    added private to Parent data.

--- src/main/java/org/example/models/Student.java
+++ src/main/java/org/example/models/Student.java
@@ -1,7 +1,7 @@
 package org.example.models;
 
 public class Student extends Person{
-    Parent parent;
+    private Parent parent;
     public Student(String firstName, String lastName, String dateOfBirth, String email, String phoneNumber, Parent parent) {
         super(firstName, lastName, dateOfBirth, email, phoneNumber);
         this.parent = parent;
