ebe8685511ae2f8b3d6375e7fc0452420cc3a86c
GFA03 <florinalexandru302003@gmail.com>
2024-04-06 10:48:31 +0300

    added Student class

---
+++ src/main/java/org/example/models/Student.java
@@ -0,0 +1,9 @@
+package org.example.models;
+
+public class Student extends Person{
+    Parent parent;
+    public Student(String firstName, String lastName, String dateOfBirth, String email, String phoneNumber, Parent parent) {
+        super(firstName, lastName, dateOfBirth, email, phoneNumber);
+        this.parent = parent;
+    }
+}
