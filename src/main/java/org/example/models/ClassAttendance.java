c6f8f663e02adb46de2550889575da89c91404ea
GFA03 <florinalexandru302003@gmail.com>
2024-04-07 10:47:38 +0300

    updated ClassAttendance constructor to not have the id in its parameters

--- src/main/java/org/example/models/ClassAttendance.java
+++ src/main/java/org/example/models/ClassAttendance.java
@@ -1,5 +1,7 @@
 package org.example.models;
 
+import java.util.Objects;
+
 public class ClassAttendance {
     private final Long id;
     private static Long ID_SEQ = 1L;
@@ -8,11 +10,60 @@ public class ClassAttendance {
     boolean present;
     int grade;
 
-    public ClassAttendance(Long id, Student student, ClassSession classSession, boolean present, int grade) {
+    public ClassAttendance(Student student, ClassSession classSession, boolean present, int grade) {
         this.id = ID_SEQ++;
         this.student = student;
         this.classSession = classSession;
         this.present = present;
         this.grade = grade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public ClassSession getClassSession() {
+        return classSession;
+    }
+
+    public void setClassSession(ClassSession classSession) {
+        this.classSession = classSession;
+    }
+
+    public boolean isPresent() {
+        return present;
+    }
+
+    public void setPresent(boolean present) {
+        this.present = present;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ClassAttendance that = (ClassAttendance) o;
+        return present == that.present && grade == that.grade && Objects.equals(id, that.id) && Objects.equals(student, that.student) && Objects.equals(classSession, that.classSession);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, student, classSession, present, grade);
     }
 }
