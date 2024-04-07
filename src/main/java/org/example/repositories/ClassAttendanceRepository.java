471408cf4c53ad7800a8ebded167254d3ec78edb
GFA03 <florinalexandru302003@gmail.com>
2024-04-07 10:48:53 +0300

    Added ClassAttendanceRepository and tests for it

---
+++ src/main/java/org/example/repositories/ClassAttendanceRepository.java
@@ -0,0 +1,63 @@
+package org.example.repositories;
+
+import org.example.models.ClassAttendance;
+import org.example.models.ClassSession;
+import org.example.models.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassAttendanceRepository implements GenericRepository<ClassAttendance>{
+    private final List<ClassAttendance> classAttendances;
+
+    public ClassAttendanceRepository() {
+        classAttendances = new ArrayList<>();
+    }
+
+    public void add(ClassAttendance classAttendance) {
+        classAttendances.add(classAttendance);
+    }
+
+    public List<ClassAttendance> getAll() {
+        return new ArrayList<>(classAttendances);
+    }
+
+    public ClassAttendance get(Long id) {
+        for (ClassAttendance classAttendance : classAttendances) {
+            if (classAttendance.getId().equals(id))
+                return classAttendance;
+        }
+        return null;
+    }
+    public ClassAttendance get(Student student, ClassSession classSession) {
+        for (ClassAttendance classAttendance : classAttendances) {
+            if (classAttendance.getStudent().equals(student) && classAttendance.getClassSession().equals(classSession))
+                return classAttendance;
+        }
+        return null;
+    }
+
+    public void update(ClassAttendance updatedClassAttendance) {
+        for (int i = 0; i < classAttendances.size(); i++) {
+            ClassAttendance classAttendance = classAttendances.get(i);
+            if (classAttendance.getStudent().equals(updatedClassAttendance.getStudent())
+                    && classAttendance.getClassSession().equals(updatedClassAttendance.getClassSession())) {
+                classAttendances.set(i, updatedClassAttendance);
+                return;
+            }
+        }
+    }
+
+    public void delete(Long id) {
+        classAttendances.removeIf(classAttendance ->
+                classAttendance.getId().equals(id));
+    }
+    public void delete(Student student, ClassSession classSession) {
+        classAttendances.removeIf(classAttendance ->
+                classAttendance.getStudent().equals(student) && classAttendance.getClassSession().equals(classSession));
+    }
+
+    public int getSize() {
+        return classAttendances.size();
+    }
+}
