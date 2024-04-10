effde54179c6b866ac85b253c2614805d5c374b6
GFA03 <florinalexandru302003@gmail.com>
2024-04-10 17:32:39 +0300

    added equals, hashcode and toString for Group model

--- src/main/java/org/example/models/Group.java
+++ src/main/java/org/example/models/Group.java
@@ -1,5 +1,7 @@
 package org.example.models;
 
+import java.util.Objects;
+
 public class Group {
     private final Long id;
     private static Long ID_SEQ = 1L;
@@ -20,5 +22,26 @@ public class Group {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Group group = (Group) o;
+        return Objects.equals(id, group.id) && Objects.equals(name, group.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
