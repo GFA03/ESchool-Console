79082bdfafb65144e4b0b80cd1d3c8f8e61e7546
GFA03 <florinalexandru302003@gmail.com>
2024-04-10 13:22:04 +0300

    added create with all parameters and update for Name and SessionDate field in ClassSession Repository and Service

--- src/main/java/org/example/repositories/ClassSessionRepository.java
+++ src/main/java/org/example/repositories/ClassSessionRepository.java
@@ -1,6 +1,8 @@
 package org.example.repositories;
 
 import org.example.models.ClassSession;
+import org.example.models.Course;
+import org.example.models.Group;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,14 +14,21 @@ public class ClassSessionRepository implements GenericRepository<ClassSession> {
         classes = new ArrayList<>();
     }
 
+    @Override
     public void add(ClassSession cls) {
         classes.add(cls);
     }
 
+    public void create(String name, Course course, Group group, String sessionDate) {
+        classes.add(new ClassSession(name, course, group, sessionDate));
+    }
+
+    @Override
     public List<ClassSession> getAll() {
         return classes;
     }
 
+    @Override
     public ClassSession get(Long classId) {
         for (ClassSession cls : classes) {
             if (cls.getId().equals(classId))
@@ -28,6 +37,7 @@ public class ClassSessionRepository implements GenericRepository<ClassSession> {
         return null;
     }
 
+    @Override
     public void update(ClassSession updatedClass) {
         for (int i = 0; i < classes.size(); i++) {
             if (classes.get(i).getId().equals(updatedClass.getId())) {
@@ -37,10 +47,32 @@ public class ClassSessionRepository implements GenericRepository<ClassSession> {
         }
     }
 
+    public void updateName(ClassSession classSession, String name) {
+        classSession.setName(name);
+        update(classSession);
+    }
+
+    public void updateCourse(ClassSession classSession, Course course) {
+        classSession.setCourse(course);
+        update(classSession);
+    }
+
+    public void updateGroup(ClassSession classSession, Group group) {
+        classSession.setGroup(group);
+        update(classSession);
+    }
+
+    public void updateSessionDate(ClassSession classSession, String sessionDate) {
+        classSession.setSessionDate(sessionDate);
+        update(classSession);
+    }
+
+    @Override
     public void delete(Long classId) {
         classes.removeIf(cls -> cls.getId().equals(classId));
     }
 
+    @Override
     public int getSize() {
         return classes.size();
     }
--- src/main/java/org/example/services/ClassSessionService.java
+++ src/main/java/org/example/services/ClassSessionService.java
@@ -1,6 +1,8 @@
 package org.example.services;
 
 import org.example.models.ClassSession;
+import org.example.models.Course;
+import org.example.models.Group;
 import org.example.repositories.ClassSessionRepository;
 
 import java.util.List;
@@ -16,6 +18,9 @@ public class ClassSessionService {
         classSessionRepository.add(classSession);
     }
 
+    public void createClassSession(String name, Course course, Group group, String sessionDate) {
+        classSessionRepository.create(name, course, group, sessionDate);
+    }
     public ClassSession getClassSessionById(Long id) {
         return classSessionRepository.get(id);
     }
@@ -28,7 +33,19 @@ public class ClassSessionService {
         classSessionRepository.update(updatedClassSession);
     }
 
+    public void updateClassSessionName(ClassSession classSession, String name) {
+        classSessionRepository.updateName(classSession, name);
+    }
+
+    public void updateClassSessionSessionDate(ClassSession classSession, String sessionDate) {
+        classSessionRepository.updateSessionDate(classSession, sessionDate);
+    }
+
     public void deleteClassSession(Long id) {
         classSessionRepository.delete(id);
+    }
+
+    public int getSize() {
+        return classSessionRepository.getSize();
     }
 }
