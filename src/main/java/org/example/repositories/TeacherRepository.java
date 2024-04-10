6861af914630db5630083e75d2ef3fbba5c7a728
GFA03 <florinalexandru302003@gmail.com>
2024-04-10 12:52:11 +0300

    added create instance with all parameters and update all parameters for Teacher Repository and Service

--- src/main/java/org/example/repositories/TeacherRepository.java
+++ src/main/java/org/example/repositories/TeacherRepository.java
@@ -1,5 +1,7 @@
 package org.example.repositories;
 
+import org.example.exceptions.InvalidEmail;
+import org.example.models.Student;
 import org.example.models.Teacher;
 
 import java.util.ArrayList;
@@ -11,15 +13,21 @@ public class TeacherRepository implements GenericRepository<Teacher> {
     public TeacherRepository() {
         teachers = new ArrayList<>();
     }
-
+    @Override
     public void add(Teacher teacher) {
         teachers.add(teacher);
     }
 
+    public void create(String firstName, String lastName, String dateOfBirth, String email, String phoneNumber) {
+        teachers.add(new Teacher(firstName, lastName, dateOfBirth, email, phoneNumber));
+    }
+
+    @Override
     public List<Teacher> getAll() {
         return teachers;
     }
 
+    @Override
     public Teacher get(Long teacherId) {
         for (Teacher teacher : teachers) {
             if (teacher.getId().equals(teacherId))
@@ -28,6 +36,7 @@ public class TeacherRepository implements GenericRepository<Teacher> {
         return null;
     }
 
+    @Override
     public void update(Teacher updatedTeacher) {
         for (int i = 0; i < teachers.size(); i++) {
             if (teachers.get(i).getId().equals(updatedTeacher.getId())) {
@@ -37,11 +46,40 @@ public class TeacherRepository implements GenericRepository<Teacher> {
         }
     }
 
+    public void updateFirstName(Teacher teacher, String firstName) {
+        teacher.setFirstName(firstName);
+        update(teacher);
+    }
+
+    public void updateLastName(Teacher teacher, String lastName) {
+        teacher.setLastName(lastName);
+        update(teacher);
+    }
+
+    public void updateDateOfBirth(Teacher teacher, String dateOfBirth) {
+        teacher.setDateOfBirth(dateOfBirth);
+        update(teacher);
+    }
+
+    public void updateEmail(Teacher teacher, String email) {
+        teacher.setEmail(email);
+        update(teacher);
+    }
+
+    public void updatePhoneNumber(Teacher teacher, String phoneNumber) {
+        teacher.setPhoneNumber(phoneNumber);
+        update(teacher);
+    }
+
+
+    @Override
     public void delete(Long teacherId) {
         teachers.removeIf(teacher -> teacher.getId().equals(teacherId));
     }
 
+    @Override
     public int getSize() {
         return teachers.size();
     }
+
 }
--- src/main/java/org/example/services/TeacherService.java
+++ src/main/java/org/example/services/TeacherService.java
@@ -16,6 +16,10 @@ public class TeacherService {
         teacherRepository.add(teacher);
     }
 
+    public void createTeacher(String firstName, String lastName, String dateOfBirth, String email, String phoneNumber) {
+        teacherRepository.create(firstName, lastName, dateOfBirth, email, phoneNumber);
+    }
+
     public Teacher getTeacherById(Long id) {
         return teacherRepository.get(id);
     }
@@ -28,7 +32,16 @@ public class TeacherService {
         teacherRepository.update(updatedTeacher);
     }
 
+    public void updateTeacherFirstName(Teacher teacher, String firstName) { teacherRepository.updateFirstName(teacher, firstName);}
+    public void updateTeacherLastName(Teacher teacher, String lastName) { teacherRepository.updateLastName(teacher, lastName);}
+    public void updateTeacherDateOfBirth(Teacher teacher, String dateOfBirth) { teacherRepository.updateDateOfBirth(teacher, dateOfBirth);}
+    public void updateTeacherEmail(Teacher teacher, String email) { teacherRepository.updateEmail(teacher, email);}
+    public void updateTeacherPhoneNumber(Teacher teacher, String phoneNumber) { teacherRepository.updatePhoneNumber(teacher, phoneNumber);}
     public void deleteTeacher(Long id) {
         teacherRepository.delete(id);
+    }
+
+    public int getSize() {
+        return teacherRepository.getSize();
     }
 }
