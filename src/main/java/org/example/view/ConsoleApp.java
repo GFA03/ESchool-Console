21db8eff52400e2644374c28172bddce27906603
GFA03 <florinalexandru302003@gmail.com>
2024-04-10 16:54:12 +0300

    added group management in console app

--- src/main/java/org/example/view/ConsoleApp.java
+++ src/main/java/org/example/view/ConsoleApp.java
@@ -108,7 +108,7 @@ public class ConsoleApp {
                 // Implement manageGroupCourses method
                 break;
             case 8:
-                // Implement manageGroups method
+                manageGroups();
                 break;
             case 9:
                 System.out.println("Exiting...");
@@ -1071,6 +1071,103 @@ public class ConsoleApp {
             System.out.println("Class attendance deleted successfully!");
         } else {
             System.out.println("Class attendance ID incorrect!");
+        }
+    }
+
+    private void manageGroups() {
+        while (true) {
+            showGroupsMenu();
+            try {
+                int option = readOption();
+                int status = executeGroupsOptions(option);
+                if (status == -1)
+                    break;
+            } catch (InvalidOption | InvalidId e) {
+                System.out.println(e.toString());
+            }
+        }
+    }
+
+    private void showGroupsMenu() {
+        System.out.println("Groups menu:");
+        System.out.println("1. Show all groups");
+        System.out.println("2. Show group by ID");
+        System.out.println("3. Create group");
+        System.out.println("4. Update group");
+        System.out.println("5. Delete group by ID");
+        System.out.println("9. Exit");
+    }
+
+    private int executeGroupsOptions(int option) throws InvalidId {
+        switch (option) {
+            case 1:
+                showAllGroups();
+                break;
+            case 2:
+                showGroupById();
+                break;
+            case 3:
+                createGroup();
+                System.out.println("Group created successfully!");
+                break;
+            case 4:
+                updateGroup();
+                break;
+            case 5:
+                deleteGroupById();
+                break;
+            case 9:
+                System.out.println("Exiting...");
+                return -1;
+            default:
+                System.out.println("Invalid choice. Please enter a valid option.");
+        }
+        return 0;
+    }
+
+    private void showAllGroups() {
+        System.out.println(groupService.getAllGroups());
+    }
+
+    private void showGroupById() throws InvalidId {
+        System.out.println("Enter group ID: ");
+        Long groupId = readLong();
+        Group group = groupService.getGroupById(groupId);
+        if (group != null)
+            System.out.println(group);
+        else
+            System.out.println("Group doesn't exist!");
+    }
+
+    private void createGroup() {
+        System.out.println("Enter group name:");
+        String groupName = scanner.nextLine();
+        groupService.createGroup(groupName);
+    }
+
+    private void updateGroup() throws InvalidId {
+        System.out.println("Enter group ID to update: ");
+        Long groupId = readLong();
+        Group group = groupService.getGroupById(groupId);
+        if (group != null) {
+            System.out.println("Enter updated group name:");
+            String groupName = scanner.nextLine();
+            groupService.updateGroupName(group, groupName);
+            System.out.println("Group updated successfully!");
+        } else {
+            System.out.println("Group ID incorrect!");
+        }
+    }
+
+    private void deleteGroupById() throws InvalidId {
+        System.out.println("Enter group ID:");
+        Long groupId = readLong();
+        Group group = groupService.getGroupById(groupId);
+        if (group != null) {
+            groupService.deleteGroup(groupId);
+            System.out.println("Group deleted successfully!");
+        } else {
+            System.out.println("Group ID incorrect!");
         }
     }
 
