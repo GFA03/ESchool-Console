f8b64a4173412fcb95e274191976e252860e8775
GFA03 <florinalexandru302003@gmail.com>
2024-04-24 15:40:59 +0300

    fix: Refactored GroupView in ConsoleApp

--- src/main/java/org/example/view/ConsoleApp.java
+++ src/main/java/org/example/view/ConsoleApp.java
@@ -468,101 +468,10 @@ public class ConsoleApp {
     }
 
     private void manageGroups() {
-        while (true) {
-            showGroupsMenu();
-            try {
-                int option = readOption();
-                int status = executeGroupsOptions(option);
-                if (status == -1)
-                    break;
-            } catch (InvalidOption | InvalidId e) {
-                System.out.println(e.toString());
-            }
-        }
+        GroupView view = new GroupView(groupService);
+        view.run();
     }
 
-    private void showGroupsMenu() {
-        System.out.println("Groups menu:");
-        System.out.println("1. Show all groups");
-        System.out.println("2. Show group by ID");
-        System.out.println("3. Create group");
-        System.out.println("4. Update group");
-        System.out.println("5. Delete group by ID");
-        System.out.println("9. Exit");
-    }
-
-    private int executeGroupsOptions(int option) throws InvalidId {
-        switch (option) {
-            case 1:
-                showAllGroups();
-                break;
-            case 2:
-                showGroupById();
-                break;
-            case 3:
-                createGroup();
-                System.out.println("Group created successfully!");
-                break;
-            case 4:
-                updateGroup();
-                break;
-            case 5:
-                deleteGroupById();
-                break;
-            case 9:
-                System.out.println("Exiting...");
-                return -1;
-            default:
-                System.out.println("Invalid choice. Please enter a valid option.");
-        }
-        return 0;
-    }
-
-    private void showAllGroups() {
-        System.out.println(groupService.getAllGroups());
-    }
-
-    private void showGroupById() throws InvalidId {
-        System.out.println("Enter group ID: ");
-        Long groupId = readLong();
-        Group group = groupService.getGroupById(groupId);
-        if (group != null)
-            System.out.println(group);
-        else
-            System.out.println("Group doesn't exist!");
-    }
-
-    private void createGroup() {
-        System.out.println("Enter group name:");
-        String groupName = scanner.nextLine();
-        groupService.createGroup(groupName);
-    }
-
-    private void updateGroup() throws InvalidId {
-        System.out.println("Enter group ID to update: ");
-        Long groupId = readLong();
-        Group group = groupService.getGroupById(groupId);
-        if (group != null) {
-            System.out.println("Enter updated group name:");
-            String groupName = scanner.nextLine();
-            groupService.updateGroupName(group, groupName);
-            System.out.println("Group updated successfully!");
-        } else {
-            System.out.println("Group ID incorrect!");
-        }
-    }
-
-    private void deleteGroupById() throws InvalidId {
-        System.out.println("Enter group ID:");
-        Long groupId = readLong();
-        Group group = groupService.getGroupById(groupId);
-        if (group != null) {
-            groupService.deleteGroup(groupId);
-            System.out.println("Group deleted successfully!");
-        } else {
-            System.out.println("Group ID incorrect!");
-        }
-    }
 
     private void manageGroupCourses() {
         while (true) {
